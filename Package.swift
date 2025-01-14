c89a08c2b6c2531cb76e4d91ba5d1f8723931ee7
Kangwook Lee <43847427+kvngwxxk@users.noreply.github.com>
2025-01-14 13:10:57 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,21 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "AlignedCollectionViewFlowLayout",
+    platforms: [
+        .iOS(.v11) // 최소 지원 버전
+    ],
     products: [
-        .library(name: "AlignedCollectionViewFlowLayout", targets: ["AlignedCollectionViewFlowLayout"])
+        .library(
+            name: "AlignedCollectionViewFlowLayout",
+            targets: ["AlignedCollectionViewFlowLayout"]
+        )
     ],
     targets: [
         .target(
             name: "AlignedCollectionViewFlowLayout",
-            path: "AlignedCollectionViewFlowLayout"
+            path: "AlignedCollectionViewFlowLayout" // 소스 코드 디렉토리 경로
         )
     ]
 )
